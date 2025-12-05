98095e135c069973dfd2978ff6df6cc9145e7ce3
Alexandra-UH <alexandra.uribe@uc.cl>
2025-12-05 19:54:27 -0300

    A1.4 Zscore
    comprobar funcionamiento

--- tests/test_statistics_utils.py
+++ tests/test_statistics_utils.py
@@ -107,6 +107,13 @@ class TestStatisticsUtils(unittest.TestCase):
         - Verificar que la media del resultado es aproximadamente 0 (usar self.assertAlmostEqual para un solo valor numérico - unittest es suficiente)
         - Verificar que la desviación estándar del resultado es aproximadamente 1 (usar self.assertAlmostEqual para un solo valor numérico - unittest es suficiente)
         """
+        utils = StatisticsUtils()
+        arr = [5,15,25,35]
+
+        result = utils.zscore(arr)
+
+        self.assesertAlmostEqual(result.mean(), 0.0, places =7)
+        self.assertAlmostEqual(result.std(), 1.0, places =7)
 
     def test_zscore_raises_for_zero_std(self):
         """Test que verifica que el método zscore lanza un ValueError cuando
